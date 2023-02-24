3d329cf33cd4501101f6ed7dc81a1b3035298913
mmkholbekov <120006110+mmkholbekov@users.noreply.github.com>
2023-02-24 21:52:46 +0400

    "Hw4 Django"

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('book_link', views.book_view, name='book'),
     path('book_link/<int:id>/', views.book_detail_view, name='details'),
+    path('book_link/<int:id>/update/', views.update_book_view, name='update'),
+    path('book_link/<int:id>/delete/', views.delete_book_view, name='delete'),
+    path('add-book/', views.create_book_view, name='create'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
 
 
 def book_view(request):
@@ -11,3 +12,41 @@ def book_view(request):
 def book_detail_view(request, id):
     book_id = get_object_or_404(models.Book, id=id)
     return render(request, 'book_detail.html', {'book_id': book_id})
+
+
+# Добавление книги через формы
+def create_book_view(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('<h2>Книга успешно добавлена!!!</h2>')
+    else:
+        form = forms.BookForm()
+    return render(request, 'add_book.html', {'form': form})
+
+
+# Изменение данных о книге
+
+def update_book_view(request, id):
+    book_object = get_object_or_404(models.Book, id=id)
+    if request.method == 'POST':
+        form = forms.BookForm(instance=book_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('<h2>Книга успешно обновлена!</h2>')
+    else:
+        form = forms.BookForm(instance=book_object)
+
+    return render(request, 'update_book.html', {
+        'form': form,
+        'object': book_object
+    })
+
+
+# Удаление из базы
+def delete_book_view(request, id):
+    show_object = get_object_or_404(models.Book, id=id)
+    show_object.delete()
+    return HttpResponse('<h2>Фильм успешно удален</h2>')
